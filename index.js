4ed9a9de8e5d5bcab6d7d746fe02cd7cf2986490
Rubén Álvarez González <rubenag83@gmail.com>
2021-01-04 20:18:29 +0100

    Se sube el proyecto del Roster

---
+++ index.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+const xlsxFile = require('read-excel-file/node');
+const fs = require('fs');
+
+var csv = "username,first name,last name,attendee email,attended,attendee company";
+var posicionesColumnas = {nombre:0, apellidos:0,email:0, empresa:0};
+var fileName = process.argv.slice(2)[0];
+
+console.log("Fichero a procesar: ", fileName);
+
+if (fileName.toLowerCase() == "ayuda"){
+    console.log("Aquí está: las columnas del Excel deben ser (nombre, apellidos, email, empresa)");
+    return;
+}
+
+var rowPos = 0;
+xlsxFile(fileName).then((rows) => {
+    for (colPos in rows[0]){
+        switch (rows[0][colPos]) {
+            case "nombre":
+                posicionesColumnas.nombre = colPos;
+                break;
+            case "apellidos":
+                posicionesColumnas.apellidos = colPos;
+                break;
+            case "email":
+                posicionesColumnas.email = colPos;
+                break;
+            case "empresa":
+                posicionesColumnas.empresa = colPos;
+                break;                
+            default:
+                break;
+        }
+    }
+
+    for (i in rows){
+        if (i > 0){
+            csv = csv.concat("\n").concat(rows[i][posicionesColumnas.email].trim() + "," + rows[i][posicionesColumnas.nombre].trim() + "," + rows[i][posicionesColumnas.apellidos].trim() + "," + rows[i][posicionesColumnas.email].trim() + ",-," + rows[i][posicionesColumnas.empresa].trim())
+        }
+    }
+    
+    console.log("COLUMNAS DETECTADAS EN LAS POSICIONES:")
+    console.log("\tNombre: ", posicionesColumnas.nombre);
+    console.log("\tApellidos: ", posicionesColumnas.apellidos);
+    console.log("\tEmail: ", posicionesColumnas.email);
+    console.log("\tEmpresa: ", posicionesColumnas.empresa);
+
+    const fileData = new Uint8Array(Buffer.from(csv));
+    fs.writeFile("roster.csv", fileData, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("¡Roster creado!");
+    }); 
+});
